7cb9df5c02418608b196290f174a12033ee2750d
Holykael1 <raptorzinho@hotmail.com>
2015-06-08 18:09:35 +0100

    final commit 2

--- core/src/com/lpoo/pokemon/screen/GameOverScreen.java
+++ core/src/com/lpoo/pokemon/screen/GameOverScreen.java
@@ -2,6 +2,7 @@ package com.lpoo.pokemon.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.lpoo.pokemon.MainGame;
@@ -9,35 +10,42 @@ import com.lpoo.pokemon.TextureManager;
 import com.lpoo.pokemon.camera.OrthoCamera;
 
 public class GameOverScreen extends Screen {
-	
+	private Music vic;
 	private OrthoCamera cam;
 	private Texture texture;
-	
+
 	public GameOverScreen(boolean won) {
-		if(won)
-			texture=TextureManager.PLAYER1WIN;
-		else 
-			texture=TextureManager.PLAYER2WIN;
+		vic = Gdx.audio.newMusic(Gdx.files.internal("victory.mp3"));
+		vic.setVolume(0.3f);
+		vic.play();
+
+		if (won)
+			texture = TextureManager.PLAYER1WIN;
+		else
+			texture = TextureManager.PLAYER2WIN;
 	}
 
 	@Override
 	public void create() {
 		cam = new OrthoCamera();
-		cam.resize(); 
+		cam.resize();
 	}
 
 	@Override
 	public void update() {
 		cam.update();
-		if(Gdx.input.isKeyPressed(Keys.ENTER))
+		if (Gdx.input.isKeyPressed(Keys.ENTER)) {
+			vic.dispose();
 			ScreenManager.setScreen(new MainMenuScreen());
+		}
 	}
 
 	@Override
 	public void render(SpriteBatch sb) {
 		sb.setProjectionMatrix(cam.combined);
 		sb.begin();
-		sb.draw(texture, MainGame.WIDTH/2-texture.getWidth()/2, MainGame.HEIGHT/2-texture.getHeight()/2);
+		sb.draw(texture, MainGame.WIDTH / 2 - texture.getWidth() / 2,
+				MainGame.HEIGHT / 2 - texture.getHeight() / 2);
 		sb.end();
 	}
 
